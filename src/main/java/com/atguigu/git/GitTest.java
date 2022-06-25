0f122f29d5f100ebeaf504c370fe56a68b96acec
lxs <1830882208@qq.com>
2022-06-25 09:36:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @author lxs
+ * @create 2022-06-25 9:31
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+
+    }
+
+}
